18445034c55389d79c226f5777dc626ca0b19956
Milomir Babic <>
2022-06-25 14:22:24 +0200

    Add tmux setup

---
+++ tmux/setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+relative=$(dirname "$0")
+# shellcheck source=../setup_utils.sh
+source "$relative/../setup_utils.sh"
+
+inform "Setting up tmux"
+
+install_dependencies "$relative/deps.txt"
+
+inform "Installing tmux-picker"
+if [[ ! -d ~/.oh-my-zsh ]]; then
+    mkdir -p ~/.tmux
+    git clone https://github.com/pawel-wiejacha/tmux-picker ~/.tmux/tmux-picker
+else
+    inform "tmux-picker already installed"
+fi
+
+install_link "$relative/.tmux.conf" "$HOME/.tmux.conf"
