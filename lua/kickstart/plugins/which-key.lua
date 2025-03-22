dcb29e5270f84cb19236ee8d27e4d1d41f5a8e11
Milomir Babic <redant333@gmail.com>
2025-03-22 21:09:27 +0100

    Update which-key settings
    It's nice to have the delay when you know what you are typing.
    Nerd font in which-key dialog just looks cluttered.

--- lua/kickstart/plugins/which-key.lua
+++ lua/kickstart/plugins/which-key.lua
@@ -19,10 +19,10 @@ return {
     opts = {
       -- delay between pressing a key and opening which-key (milliseconds)
       -- this setting is independent of vim.opt.timeoutlen
-      delay = 0,
+      delay = 500,
       icons = {
         -- set icon mappings to true if you have a Nerd Font
-        mappings = vim.g.have_nerd_font,
+        mappings = false,
         -- If you are using a Nerd Font: set icons.keys to an empty table which will use the
         -- default which-key.nvim defined Nerd Font icons, otherwise define a string table
         keys = vim.g.have_nerd_font and {} or {
