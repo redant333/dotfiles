84f4d4745a0377fda214b8d5fe3aaf298345d86e
Milomir Babic <redant333@gmail.com>
2025-03-23 18:18:33 +0100

    Add modified conform

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -31,7 +31,7 @@ require('lazy').setup({
 
   require 'kickstart/plugins/lspconfig',
 
-  -- require 'kickstart/plugins/conform',
+  require 'kickstart/plugins/conform',
 
   require 'kickstart/plugins/cmp',
 
